1f81ee5b1d94c26b80937f4b24fa096f8b5245f7
smooney211993 <sm.athome21@gmail.com>
2020-09-20 14:03:21 +1000

    added in the arguments for the google strat

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('Server is running');
 });
-//passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
