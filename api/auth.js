7533c49fbb1caac5e28571e8af12de03fee66b8c
smooney211993 <sm.athome21@gmail.com>
2020-09-20 15:02:48 +1000

    create access to profile and email

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const passport = require('passport');
+const keys = require('../config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(
   new GoogleStrategy(
@@ -21,3 +22,5 @@ router.get(
     scope: ['profile', 'email'],
   })
 );
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT || 3000;
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+const auth = require('./api/auth');
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('Server is running');
 });
+
+app.use('/api/auth', auth);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
