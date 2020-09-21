ad787eef3362f6e675ff2202736493dfc997a475
smooney211993 <sm.athome21@gmail.com>
2020-09-22 09:03:10 +1000

    created the user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = User = mongoose.models('user', UserSchema);
