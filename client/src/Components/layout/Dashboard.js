a2e653eaa0f771948e28494c03cbcde25cdac6b4
smooney211993 <sm.athome21@gmail.com>
2020-09-23 13:14:34 +1000

    display flex on header in dashboard

--- client/src/Components/layout/Dashboard.js
+++ client/src/Components/layout/Dashboard.js
@@ -7,21 +7,18 @@ const Dashboard = ({ user }) => {
     <>
       <header id='main-header' className='py-2 bg-primary text-white'>
         <div className='container'>
-          <div className='row'>
-            <div className='col-md-6'>
-              <h1>
-                {' '}
-                <i className='fas fa-envelope-open-text'></i> Dashboard
-              </h1>
-            </div>
+          <div className='d-flex justify-content-around'>
+            <h1>
+              {' '}
+              <i className='fas fa-envelope-open-text'></i> Dashboard
+            </h1>
+            <p className='h4 display mt-3 ml-auto'>
+              <i className='fa fa-user'></i>Hello {user && user.firstName}
+            </p>
           </div>
         </div>
       </header>
-      <div className='container'>
-        <p className='h4 display mt-3'>
-          <i className='fa fa-user'></i>Hello {user && user.firstName}
-        </p>
-      </div>
+      <div className='container'></div>
     </>
   );
 };
